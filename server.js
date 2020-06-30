cd3c022fa4a7264062223c836ce4fe5f8b03d308
Jesus Romero <jhromero@tekmexico.com>
2020-06-30 16:02:46 -0700

    feat!: commenting all code out and starting from scratch implementing all functionality.

--- server.js
+++ server.js
@@ -5,11 +5,20 @@ const PORT = process.env.PORT || 3000;
 const express = require('express');
 const socketIO = require('socket.io');
 
-var app = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = express()
+    .use(((req, res) => res.write('WEBSOCKET SERVER FOR WEBRTC SIGNALING')))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+// var app = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // socket.io goes below
 
-var io = socketIO(app);
+const io = socketIO(server);
+
+io.on('connection', socket => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+});
 
 // io.listen({
 //     log: true,
@@ -22,66 +31,66 @@ var io = socketIO(app);
 //     'jsonp-polling'
 // ]);
 
-var channels = {};
+// var channels = {};
 
-io.on('connection', function (socket) {
-    var initiatorChannel = '';
-    if (!io.isConnected) {
-        io.isConnected = true;
-    }
+// io.on('connection', function (socket) {
+//     var initiatorChannel = '';
+//     if (!io.isConnected) {
+//         io.isConnected = true;
+//     }
 
-    socket.on('new-channel', function (data) {
-        if (!channels[data.channel]) {
-            initiatorChannel = data.channel;
-        }
+//     socket.on('new-channel', function (data) {
+//         if (!channels[data.channel]) {
+//             initiatorChannel = data.channel;
+//         }
 
-        channels[data.channel] = data.channel;
-        onNewNamespace(data.channel, data.sender);
-    });
+//         channels[data.channel] = data.channel;
+//         onNewNamespace(data.channel, data.sender);
+//     });
 
-    socket.on('presence', function (channel) {
-        var isChannelPresent = !!channels[channel];
-        socket.emit('presence', isChannelPresent);
-    });
+//     socket.on('presence', function (channel) {
+//         var isChannelPresent = !!channels[channel];
+//         socket.emit('presence', isChannelPresent);
+//     });
 
-    socket.on('disconnect', function (channel) {
-        if (initiatorChannel) {
-            delete channels[initiatorChannel];
-        }
-    });
-});
+//     socket.on('disconnect', function (channel) {
+//         if (initiatorChannel) {
+//             delete channels[initiatorChannel];
+//         }
+//     });
+// });
 
-function onNewNamespace(channel, sender) {
-    io.of('/' + channel).on('connection', function (socket) {
-        var username;
-        if (io.isConnected) {
-            io.isConnected = false;
-            socket.emit('connect', true);
-        }
+// function onNewNamespace(channel, sender) {
+//     io.of('/' + channel).on('connection', function (socket) {
+//         var username;
+//         if (io.isConnected) {
+//             io.isConnected = false;
+//             socket.emit('connect', true);
+//         }
 
-        socket.on('message', function (data) {
-            if (data.sender == sender) {
-                if (!username) username = data.data.sender;
+//         socket.on('message', function (data) {
+//             if (data.sender == sender) {
+//                 if (!username) username = data.data.sender;
 
-                socket.broadcast.emit('message', data.data);
-            }
-        });
+//                 socket.broadcast.emit('message', data.data);
+//             }
+//         });
 
-        socket.on('disconnect', function () {
-            if (username) {
-                socket.broadcast.emit('user-left', username);
-                username = null;
-            }
-        });
-    });
-}
+//         socket.on('disconnect', function () {
+//             if (username) {
+//                 socket.broadcast.emit('user-left', username);
+//                 username = null;
+//             }
+//         });
+//     });
+// }
 
 // run app
 
 // app.listen(process.env.PORT || 9559);
 
-process.on('unhandledRejection', (reason, promise) => {
-    process.exit(1);
-});
+// process.on('unhandledRejection', (reason, promise) => {
+//     process.exit(1);
+// });
 
 // console.log('Please open SSL URL: https://localhost:' + (process.env.PORT || 9559) + '/');
